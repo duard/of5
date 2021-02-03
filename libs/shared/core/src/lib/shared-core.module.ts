bb07e77454954c749d46a1ffecb017ca3942d810
Carlos Casa <duardbr@gmail.com>
2021-02-03 08:27:47 -0300

    feat: Configurações adicionadas

--- libs/shared/core/src/lib/shared-core.module.ts
+++ libs/shared/core/src/lib/shared-core.module.ts
@@ -1,10 +1,31 @@
-import { NgModule } from '@angular/core';
+import { Inject, ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { SharedDataAccessModule } from '@of5/shared/data-access';
+import { environment } from "@of5/shared/environments";
 
 @NgModule({
   imports: [
     SharedDataAccessModule.forRoot(),
   ],
 })
-export class SharedCoreModule {}
+export class SharedCoreModule {
+
+  isDev = !environment.production
+
+  constructor(
+    @Optional()
+    @SkipSelf()
+    parentModule?: SharedCoreModule
+  ) {
+    
+    if (this.isDev) {
+      console.log('isDev', this.isDev);
+    }
+
+    if (parentModule) {
+      throw new Error(
+        'SharedCoreModule só pode ser importada uma vez, no AppModule :-('
+      );
+    }
+  }
+}
 
