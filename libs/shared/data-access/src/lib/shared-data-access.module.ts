265abc2e90704b58d585a801fbb983c43fc6c254
Carlos Casa <duardbr@gmail.com>
2021-02-03 08:41:38 -0300

    feat: adicionando @ngrx/data

--- libs/shared/data-access/src/lib/shared-data-access.module.ts
+++ libs/shared/data-access/src/lib/shared-data-access.module.ts
@@ -1,20 +1,50 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { isDevMode, ModuleWithProviders, NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '@of5/shared/environments';
 
 import { metaReducers, reducers } from './reducers';
 
+const RouterStateMinimal = 1;
+
 @NgModule({
   imports: [
+    CommonModule,
+    HttpClientModule,
     StoreModule.forRoot(reducers, {
       metaReducers,
+      runtimeChecks: {
+        strictActionImmutability: false,
+        strictActionSerializability: false,
+        strictActionTypeUniqueness: isDevMode(),
+        strictActionWithinNgZone: isDevMode(),
+        strictStateImmutability: isDevMode(),
+        strictStateSerializability: false
+      }
     }),
-    StoreDevtoolsModule.instrument({
-      logOnly: environment.production,
-      maxAge: 25,
-    }),
+    // EffectsModule.forRoot([HydrationEffects, RouterEffects, SettingsEffects]),
+    // StoreRouterConnectingModule.forRoot({ routerState: RouterStateMinimal }),
+    // EntityDataModule.forRoot(entityConfig),
+
+    environment.production
+      ? []
+      : StoreDevtoolsModule.instrument({
+          name: '2020 Estoques',
+          maxAge: 26
+        })
   ],
+  providers: [
+    // {
+    //   provide: PersistenceResultHandler,
+    //   useClass: AdditionalPersistenceResultHandler
+    // },
+    // {
+    //   provide: EntityCollectionReducerMethodsFactory,
+    //   useClass: AdditionalEntityCollectionReducerMethodsFactory
+    // }
+  ]
 })
 export class SharedDataAccessRootModule {}
 
@@ -26,3 +56,52 @@ export class SharedDataAccessModule {
     };
   }
 }
+
+/*
+const RouterStateMinimal = 1;
+
+@NgModule({
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        // strictActionWithinNgZone: false,
+        strictActionImmutability: false,
+        strictActionSerializability: false,
+        strictActionTypeUniqueness: isDevMode(),
+        strictActionWithinNgZone: isDevMode(),
+        strictStateImmutability: isDevMode(),
+        strictStateSerializability: false
+      }
+    }),
+    EffectsModule.forRoot([HydrationEffects, RouterEffects, SettingsEffects]),
+    StoreRouterConnectingModule.forRoot({ routerState: RouterStateMinimal }),
+    EntityDataModule.forRoot(entityConfig),
+
+    environment.production
+      ? []
+      : StoreDevtoolsModule.instrument({
+          name: '2020 Estoques',
+          maxAge: 26
+        })
+  ],
+  providers: [
+    {
+      provide: PersistenceResultHandler,
+      useClass: AdditionalPersistenceResultHandler
+    },
+    {
+      provide: EntityCollectionReducerMethodsFactory,
+      useClass: AdditionalEntityCollectionReducerMethodsFactory
+    }
+  ]
+})
+export class AppStoreModule {
+  constructor() {
+    console.log('isDevMode()', isDevMode());
+  }
+}
+
+*/
