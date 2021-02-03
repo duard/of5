e340aec054d091756bd22fc2d1ea8273029fffba
Carlos Casa <duardbr@gmail.com>
2021-02-03 09:11:00 -0300

    feat: adicionando @ngrx/data

--- libs/shared/data-access/src/lib/hydration/hydration.effects.ts
+++ libs/shared/data-access/src/lib/hydration/hydration.effects.ts
@@ -7,34 +7,34 @@ import * as HydrationActions from './hydration.actions';
 
 @Injectable()
 export class HydrationEffects implements OnInitEffects {
-  hydrate$ = createEffect(() =>
-    this.action$.pipe(
-      ofType(HydrationActions.hydrate),
-      map(() => {
-        const storageValue = localStorage.getItem('state');
-        if (storageValue) {
-          try {
-            const state = JSON.parse(storageValue);
-            return HydrationActions.hydrateSuccess({ state });
-          } catch {
-            localStorage.removeItem('state');
-          }
-        }
-        return HydrationActions.hydrateFailure();
-      })
-    )
-  );
+  // hydrate$ = createEffect(() =>
+  //   this.action$.pipe(
+  //     ofType(HydrationActions.hydrate),
+  //     map(() => {
+  //       const storageValue = localStorage.getItem('state');
+  //       if (storageValue) {
+  //         try {
+  //           const state = JSON.parse(storageValue);
+  //           return HydrationActions.hydrateSuccess({ state });
+  //         } catch {
+  //           localStorage.removeItem('state');
+  //         }
+  //       }
+  //       return HydrationActions.hydrateFailure();
+  //     })
+  //   )
+  // );
 
-  serialize$ = createEffect(
-    () =>
-      this.action$.pipe(
-        ofType(HydrationActions.hydrateSuccess, HydrationActions.hydrateFailure),
-        switchMap(() => this.store),
-        distinctUntilChanged(),
-        tap(state => localStorage.setItem('state', JSON.stringify(state)))
-      ),
-    { dispatch: false }
-  );
+  // serialize$ = createEffect(
+  //   () =>
+  //     this.action$.pipe(
+  //       ofType(HydrationActions.hydrateSuccess, HydrationActions.hydrateFailure),
+  //       switchMap(() => this.store),
+  //       distinctUntilChanged(),
+  //       tap(state => localStorage.setItem('state', JSON.stringify(state)))
+  //     ),
+  //   { dispatch: false }
+  // );
 
   constructor(private action$: Actions, private store: Store<RootState>, private ngZone: NgZone) {}
 
@@ -63,7 +63,8 @@ export class HydrationEffects implements OnInitEffects {
 
     // this.hydrate();
 
-    return null;
+    return       HydrationActions.hydrate();
+    ;
   }
 
   hydrate() {
